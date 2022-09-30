23cf0ecc67053d257c3a4ec2d59af20e02668fae
aanonpro <anonphin.pro@gmail.com>
2022-09-30 18:16:54 +0700

    add project

---
+++ larave_pos/database/migrations/2022_09_30_095024_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->default('POS System');
+            $table->string('address')->default('phnom penh');
+            $table->string('phone')->default('+855 123456789');
+            $table->string('email')->default('pos@gmail.com');
+            $table->string('fax')->default('+855 90874321');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
